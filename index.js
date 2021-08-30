d75844038372f71b75fd0fd15d26e63ff81a0a9d
fede-veloper <fedev.code@gmail.com>
2021-08-30 07:08:29 -0500

    PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
