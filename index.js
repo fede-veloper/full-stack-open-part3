f443be32fc3c7ee458ee29aec31b68873b23cdbd
fede-veloper <fedev.code@gmail.com>
2021-08-31 18:11:37 -0500

    3.15-3.18

--- index.js
+++ index.js
@@ -14,29 +14,6 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(morgan(':method :url :status ms - :response-time :body'));
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123457",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-];
-
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.get('/api/persons', (req, res) => {
@@ -44,34 +21,29 @@ app.get('/api/persons', (req, res) => {
     .then(persons => res.json(persons));
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => res.json(person))
-    .catch(error => console.log('error ' + error));
+    .catch(error => next(error));
 });
 
 app.get('/info', (req, res) => {
-  res.send('<p>Phonebook has info for ' + persons.length + ' people</p>' + '<p>' + new Date() + '<p>');
+  Person.find({}).then(persons => {
+    res.send('<p>Phonebook has info for ' + persons.length + ' people</p>' + '<p>' + new Date() + '<p>');
+  });
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const person = persons.find(person => person.id === id);
-
-  if (!person)
-    return res.status(404).end();
-  persons = persons.filter(person => person.id !== id);
-  res.send(person);
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(response => res.send(response))
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (req, res) => {
   const body = req.body;
-  // const personExists = persons.find(person => person.name === body.name);
 
   if (!body.name || !body.number) 
     return res.status(400).json({error: 'no empty fields'});
-  /* if(personExists) 
-    return res.status(400).json({error: 'name must be unique'}); */
 
   const person = new Person({
     name: body.name, 
@@ -82,10 +54,27 @@ app.post('/api/persons', (req, res) => {
   res.send(person);
 });
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+  const person = {
+    name: body.name, 
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(result => res.send(result))
+    .catch(error => next(error));
+});
+
 const unkownEntry = (req, res) => {
   res.status(400).send({ error: "unknown endpoint" });
 }
 
+const handlerError = (error, req, res, next) => {
+  res.status(400).send({ error: 'error' });
+};
+
+app.use(handlerError);
 app.use(unkownEntry);
 
 app.listen(PORT, () => {
